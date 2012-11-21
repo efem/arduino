801110ae71b151e46fa49eb07ca553d3998a7e8b
Terminator <qlkid@o2.pl>
2012-11-21 23:35:08 +0100

    start

---
+++ RFID_zForum/RFID_zForum.ino
@@ -0,0 +1,19 @@
+SoftwareSerial mySerial= SoftwareSerial(rxPin, txPin);
+
+void setup()
+{
+  Serial.begin(9600);
+  mySerial.begin(9600);
+  pinMode(rxPin, INPUT);
+  pinMode(txPin, OUTPUT);
+  }
+  
+  void loop()
+  {
+    for (int i=0;i<=13;i++){
+      Serial.print(mySerial.read(),BYTE);
+      }
+      Serial.println("   <<Card ID");
+      delay(1000);
+      }
+
