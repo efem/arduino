64b1c26a9830a0737f6c1d13af0e426a30fc30e7
Terminator <qlkid@o2.pl>
2012-11-22 14:53:58 +0100

    mysql conn

--- ARD/laczenie.py
+++ ARD/laczenie.py
@@ -1,4 +1,14 @@
 import serial
+import MySQLdb
+
+conn = MySQLdb.connect("127.0.0.1", "root", "r00t", "test")
+
+
 
 print 'Zaimportowano'
 
+
+c = conn.cursor()
+c.execute("SELECT * FROM pracownicy")
+for rec in c.fetchall():
+    print "ID: %d\nNick: %s\nHaslo: %s\nKarta: %s\n" % (rec[0], rec[1], rec[2], rec[3])
