0d82e8308da2c0cd2a8b326d563b477a4cec0a54
Terminator <qlkid@o2.pl>
2012-11-27 17:23:01 +0100

    W funkcji czujnik

--- Ultrasonic_Testing/Ultrasonic_Testing.ino
+++ Ultrasonic_Testing/Ultrasonic_Testing.ino
@@ -3,16 +3,34 @@
 int pingPin = 13;
 int inPin = 12;
 int led = 8;
-
+int buttonPin = 4;
+int buttonState = 0;
+int ileCM;
 void setup() {
-Serial.begin( 9600 );
-pinMode(led, OUTPUT); 
+    Serial.begin( 9600 );
+    pinMode(led, OUTPUT);
+    pinMode(buttonPin, INPUT);
 }
 
 void loop()
 {
-  long duration, inches, cm;
-
+  buttonState = digitalRead(buttonPin);
+  //odpalCzujnik();
+  ileCM = odpalCzujnik();
+  
+  if (ileCM < 30 || buttonState == HIGH) {
+    zapal();
+  }
+  else if (ileCM >= 30 || buttonState == LOW) 
+  {
+    zgas();
+  }
+  
+}
+int odpalCzujnik()
+{
+    long duration, inches, cm;
+  
   pinMode(pingPin, OUTPUT);
   digitalWrite(pingPin, LOW);
   delayMicroseconds(2);
@@ -28,15 +46,9 @@ void loop()
   Serial.print("cm");
   Serial.println();
   delay(300);
-  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(1);
   
-  if (cm<30) {
-    zapal();
-  }
-  
+  return cm;
 }
-
 long microsecondsToCentimeters(long microseconds)
 {
   return microseconds / 29 / 2;
@@ -44,5 +56,10 @@ long microsecondsToCentimeters(long microseconds)
 void zapal()
 {
   digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1);
+}
+void zgas()
+{
+  digitalWrite(led, LOW);   // turn the LED on (HIGH is the voltage level)
   delay(1);
 }
