ff29ae26aef26a086c65b60c1e3d7b95a61cdd26
Terminator <qlkid@o2.pl>
2012-11-27 21:25:19 +0100

    wszystko pofunkcjowane - nie wiem czy ok

--- Ultrasonic_Testing/Ultrasonic_Testing.ino
+++ Ultrasonic_Testing/Ultrasonic_Testing.ino
@@ -4,22 +4,29 @@
 #define rxPin 2
 #define txPin 3
 
-SoftwareSerial rfid = SoftwareSerial(rxPin, txPin);
+SoftwareSerial RFID = SoftwareSerial(rxPin, txPin);
 
 
-int pingPin = 13;
-int inPin = 12;
-int led = 8;
-int buttonPin = 4;
-int buttonState = 0;
-int ileCM;
-const int odleglosc = 30;
+const int pingPin = 13; //do czujnika
+const int inPin = 12; //do czujnika
+const int led = 8; //niebieska dioda
+const int buttonPin = 4; //przycisk z wewnatrz
+const int odleglosc = 30; //odglosc jaka wzbudza uklad rfid (w cm)
+int buttonState = 0; //stan przycisku wew.
+int ileCM; //odczyt zujnika
+
+String msg; //TAG
+
+char c; //zbiera znaki do TAGa
+
 
 
 void setup() {
     Serial.begin( 9600 );
     pinMode(led, OUTPUT);
     pinMode(buttonPin, INPUT);
+    RFID.begin(9600);
+    Serial.println("RFID Ready");
 }
 
 void loop()
@@ -28,13 +35,26 @@ void loop()
   //odpalCzujnik();
   //ileCM = odpalCzujnik();
   
-  if (odpalCzujnik() < odleglosc || buttonState == HIGH) {
-    zapal();
-  }
-  else if (odpalCzujnik() >= odleglosc || buttonState == LOW) 
+  if (odpalCzujnik() < odleglosc)
   {
-    zgas();
+     wlaczRFID(true); //*********************************************UAKTYWNIKJ PIN OD RFID
+     RFID.flush();
+     czytajRFID();  //*********************************************************CZYTAJ RFID
   }
+  else if (odpalCzujnik() >= odleglosc)
+  {
+      wlaczRFID(false); //***********************************************WYGAS PIN OD RFID
+  }
+  
+  if (buttonState == HIGH)
+  {
+      zapal();
+  } 
+  else if (buttonState == LOW)
+  {
+      zgas();
+  }
+ 
   
 }
 int odpalCzujnik()
@@ -52,9 +72,9 @@ int odpalCzujnik()
   duration = pulseIn(inPin, HIGH);
 
   cm = microsecondsToCentimeters(duration);
-  //Serial.print(cm);
+  Serial.print(cm);
   //Serial.print("cm");
-  //Serial.println();
+  Serial.println();
   delay(300);
   
   return cm;
@@ -73,3 +93,36 @@ void zgas()
   digitalWrite(led, LOW);   // turn the LED on (HIGH is the voltage level)
   delay(1);
 }
+void wlaczRFID(boolean wlacz) {
+  if (wlacz == true) { 
+    digitalWrite(txPin, LOW); //enables the RDIF reader and turns on the diode on the arduino
+  } else {                               //disables the RDIF reader and turns off the diode on the arduino
+    digitalWrite(txPin, HIGH);   
+  }
+}
+void czytajRFID()
+{
+  
+  while(RFID.available()>0)
+         {
+            c=RFID.read(); 
+            msg += c;
+   
+            Serial.println(msg);  //przyrastajacy TAG
+            Serial.println(msg.length()); //      
+          }
+    
+    if(msg.length()>10) // DLUGOSVC WIADOMOSCI
+    {
+      wypisz();
+    }
+}
+void wypisz(){
+    msg=msg.substring(1,13);
+    Serial.print("TAG:" );
+    Serial.println(msg);
+    RFID.flush(); //czyszcze bufor
+
+}
+  
+  
