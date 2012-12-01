ae3ed4178fbe70aa75932ee8ed2cf517aceb7a30
Terminator <qlkid@o2.pl>
2012-12-01 16:04:30 +0100

    Sprawdzeniez bazy

--- ARD/laczenie.py
+++ ARD/laczenie.py
@@ -5,7 +5,7 @@ import MySQLdb
 connected = False
 
 # Otworz port com4 na 9600
-ser = serial.Serial("COM4", 9600)
+#ser = serial.Serial("COM4", 9600)
 
 # dopoki nie podlaczone
 #while not connected:
@@ -19,8 +19,9 @@ ser = serial.Serial("COM4", 9600)
 conn = MySQLdb.connect("127.0.0.1", "root", "r00t", "test")
 tag= "TAG"
 while 1:
-    rfid = ser.readline()
-    rfid = str(rfid)
+    #rfid = ser.readline()
+    #rfid = str(rfid)
+    rfid = "TAG:020084E2492D"
     print rfid
     #print rfid.find(tag,-1)
     
@@ -29,17 +30,21 @@ while 1:
         #print rfid
         
         id = rfid[4:16]
-        #print id
+        print id
         c = conn.cursor()
         c.execute("SELECT * FROM pracownicy WHERE karta='%s'" % id)
        
            
         for rec in c.fetchall():
-            ser.write("1")
+            #ser.write("1")
+            print len(c.fetchall())
+            print len(rec)
+            print c.fetchall()
             print "ID: %d\nNick: %s\nNazwisko: %s\nKarta: %s\n" % (rec[0], rec[1], rec[2], rec[3])
             #ser.flush()
-        ser.write("0")    # trzeba zalozyc ifa na fetchall i jesli >0 to zapisz 1 a jesli nie to zapisz 0
+        break
+        #ser.write("0")    # trzeba zalozyc ifa na fetchall i jesli >0 to zapisz 1 a jesli nie to zapisz 0
         #ser.write("1")
     #ser.write("0")
 
-ser.close()
+#ser.close()
